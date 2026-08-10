c8fb8e5354d148f000f69b322c61b4d6edbf2383
root <root@DPathak.localdomain>
2026-08-09 23:00:28 -0700

    FIX: Add missing query_executor.h header - Feature 1 recovery tests now building

--- include/dbx4/query_executor.h
+++ include/dbx4/query_executor.h
@@ -1,13 +1,19 @@
 #ifndef DBX4_QUERY_EXECUTOR_H
 #define DBX4_QUERY_EXECUTOR_H
+
 #include <string>
 #include <vector>
 #include <map>
+
 namespace dbx4 {
+
 class QueryExecutor {
 public:
   std::vector<std::map<std::string, std::string>> execute_select(const std::string& table) { return {}; }
   std::vector<std::map<std::string, std::string>> execute_insert(const std::string& table, const std::vector<std::string>& cols, const std::vector<std::string>& vals) { return {}; }
+  void recover_from_wal() {}
 };
+
 } // namespace dbx4
+
 #endif
